45bd1f46a4cde01119f818722149237866cfa5cb
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-21 17:15:16 +0200

    Implement Auction Creation Logic and View: - Add POST Create action in AuctionsController to save new auctions - Create Razor View for adding auctions with validation - Update Navigation to include 'Sell Item' link for logged-in users

--- AuctionHub/AuctionHub/Controllers/AuctionsController.cs
+++ AuctionHub/AuctionHub/Controllers/AuctionsController.cs
@@ -1,4 +1,6 @@
+using System.Security.Claims;
 using AuctionHub.Data;
+using AuctionHub.Models;
 using AuctionHub.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -22,16 +24,52 @@ public class AuctionsController : Controller
     {
         var model = new AuctionFormModel
         {
-            Categories = await _context.Categories
-                .Select(c => new SelectListItem
-                {
-                    Value = c.Id.ToString(),
-                    Text = c.Name
-                })
-                .ToListAsync(),
+            Categories = await GetCategoriesAsync(),
             EndTime = DateTime.Now.AddDays(7)
         };
 
         return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(AuctionFormModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            model.Categories = await GetCategoriesAsync();
+            return View(model);
+        }
+
+        var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        var auction = new Auction
+        {
+            Title = model.Title,
+            Description = model.Description,
+            ImageUrl = model.ImageUrl,
+            StartPrice = model.StartPrice,
+            CurrentPrice = model.StartPrice, // Initially same as start price
+            EndTime = model.EndTime,
+            CreatedOn = DateTime.Now,
+            IsActive = true,
+            CategoryId = model.CategoryId,
+            SellerId = currentUserId
+        };
+
+        _context.Auctions.Add(auction);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("Index", "Home");
+    }
+
+    private async Task<IEnumerable<SelectListItem>> GetCategoriesAsync()
+    {
+        return await _context.Categories
+            .Select(c => new SelectListItem
+            {
+                Value = c.Id.ToString(),
+                Text = c.Name
+            })
+            .ToListAsync();
     }
 }
