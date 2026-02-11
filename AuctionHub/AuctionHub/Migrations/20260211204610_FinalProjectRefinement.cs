a92c758f60047b6ee83cfa6cf7d7edf7c5c9c785
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-02-11 22:48:14 +0200

    🚀 Project Completion: Comprehensive feature implementation, UI/UX premium overhaul, security enhancements, and technical documentation

---
+++ AuctionHub/AuctionHub/Migrations/20260211204610_FinalProjectRefinement.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalProjectRefinement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
